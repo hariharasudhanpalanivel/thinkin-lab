934e71e17422aad2a739edb01e776347697dd4bf
harihara.palanivel <hariharasudhanpalanivel97@gmail.com>
2025-01-08 22:28:39 +0530

    second commit

---
+++ src/app/pages/projects/atl/atl.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atl',
+  imports: [],
+  templateUrl: './atl.component.html',
+  styleUrl: './atl.component.scss'
+})
+export class ATLComponent {
+
+}
